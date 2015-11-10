e27d68b27d38bfd6394348152f5ada067d520c50
luna825 <luna825@qq.com>
2015-11-10 16:55:35 +0800

    add roles data

--- app/models.py
+++ app/models.py
@@ -4,6 +4,14 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask import current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
+
+class Permission:
+	FOLLOW = 0x01
+	COMMENT = 0x02
+	WRITE_ARTICLES = 0x04
+	MODERATE_COMMENTS = 0x08
+	ADMINISTER = 0x80
+
 class User(db.Model,UserMixin):
 	__tablename__ = 'users'
 
@@ -13,6 +21,14 @@ class User(db.Model,UserMixin):
 	password_hash = db.Column(db.String(128))
 	role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 	confirmed = db.Column(db.Boolean,default=False)
+
+	def __init__(self,**kwargs):
+		super(User,self).__init__(**kwargs)
+		if self.role is None:
+			if self.email == 'luna825@qq.com':
+				self.role = Role.query.filter_by(Permissions = 0xff).first()
+			else:
+				self.role = Role.query.filter_by(default=True).first()
 
 
 	#password hash
@@ -55,9 +71,23 @@ class User(db.Model,UserMixin):
 		self.password = new_password
 		db.session.add(self)
 		return True
+
+	#role auth
+	def can(self,permission):
+		return self.role is not None and (self.role.Permissions & permission) == permission
+	def is_administrator(self):
+		return self.can(Permission.ADMINISTER)
 		
 	def __repr__(self):
 		return '<User %r>' % self.email
+
+class AnonymousUser(AnonymousUserMixin):
+	def can(self,Permission):
+		return False
+	def is_administrator(self):
+		return False
+login_manager.anonymous_user = AnonymousUser
+
 
 
 
@@ -65,7 +95,32 @@ class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer,primary_key=True)
 	name = db.Column(db.String(64),unique=True,index=True)
+	default = db.Column(db.Boolean,default=False,index = True)
+	Permissions = db.Column(db.Integer)
 	users = db.relationship('User',backref='role',lazy='dynamic')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+	@staticmethod
+	def insert_roles():
+		roles = {
+			'User':(Permission.FOLLOW|Permission.COMMENT|Permission.WRITE_ARTICLES,True),
+			'Moderator':(Permission.FOLLOW|
+						 Permission.COMMENT|
+						 Permission.WRITE_ARTICLES|
+						 Permission.MODERATE_COMMENTS,False),
+			'Administrator':(0xff,False)
+		}
+
+		for r in roles:
+			role = Role.query.filter_by(name = r).first()
+			if role is None:
+				role= Role(name = r)
+			role.Permissions = roles[r][0]
+			role.default = roles[r][1]
+			db.session.add(role)
+		db.session.commit()
 
 
 @login_manager.user_loader
