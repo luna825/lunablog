7fff9e96f1edc3f613d80a0aa09227263422f586
luna825 <luna825@qq.com>
2015-11-20 11:40:02 +0800

    完成评论功能

--- app/models.py
+++ app/models.py
@@ -44,6 +44,7 @@ class User(db.Model,UserMixin):
 	followers = db.relationship('Follow',foreign_keys=[Follow.followed_id],
 		backref=db.backref('followed',lazy='joined'),lazy='dynamic',
 		cascade='all,delete-orphan')
+	comments = db.relationship('Comment',backref='author',lazy='dynamic')
 
 
 	def __init__(self,**kwargs):
@@ -231,6 +232,7 @@ class Post(db.Model):
 	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
 	auth_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 	body_html = db.Column(db.Text)
+	comments = db.relationship('Comment',backref='post',lazy='dynamic')
 
 	@staticmethod
 	def on_changed_body(target,value,oldvalue,initiator):
@@ -257,6 +259,23 @@ class Post(db.Model):
 
 db.event.listen(Post.body,'set',Post.on_changed_body)
 
+class Comment(db.Model):
+	__tablename__='Comments'
+	id = db.Column(db.Integer,primary_key=True)
+	body = db.Column(db.Text)
+	body_html = db.Column(db.Text)
+	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+	disabled = db.Column(db.Boolean)
+	author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+	post_id = db.Column(db.Integer,db.ForeignKey('posts.id'))
+
+	@staticmethod
+	def on_change_body(target,value,oldvalue,initiator):
+		allowed_tags = ['a','abbr','acronym','b','code','em','i','strong']
+		target.body_html = bleach.linkify(bleach.clean(
+			markdown(value,output_format='html'),tags=allowed_tags,
+			strip = True))
+db.event.listen(Comment.body,'set',Comment.on_change_body)
 
 @login_manager.user_loader
 def load_user(user_id):
